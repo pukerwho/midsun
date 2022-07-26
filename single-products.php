125c8a7c2e1e69f4c8855411d5673e5a0e8293fa
Puker Who <nawwesele@gmail.com>
2022-07-26 12:40:19 +0300

    edit

--- single-products.php
+++ single-products.php
@@ -4,31 +4,31 @@
 <div class="space-top">
   <main id="primary" class="mb-20 py-10 xl:py-20">
     <div class="container">
+      <!-- Хлебные крошки -->    
+      <div class="breadcrumbs text-sm text-gray-800 mb-6" itemprop="breadcrumb" itemscope itemtype="https://schema.org/BreadcrumbList">
+        <ul class="flex items-center flex-wrap -mx-4">
+          <li itemprop='itemListElement' itemscope itemtype='https://schema.org/ListItem' class="breadcrumbs_item px-4 pl-8 ml-4">
+            <a itemprop="item" href="<?php echo home_url(); ?>" class="text-orange-400 dark:text-orange-200">
+              <span itemprop="name"><?php _e( 'Головна', 'treba-wp' ); ?></span>
+            </a>                        
+            <meta itemprop="position" content="1">
+          </li>
+          <li itemprop='itemListElement' itemscope itemtype='http://schema.org/ListItem' class="breadcrumbs_item px-4">
+            <a itemprop="item" href="<?php echo get_post_type_archive_link('products'); ?>" class="text-orange-400 dark:text-orange-200">
+              <span itemprop="name"><?php _e( 'Обладнання', 'treba-wp' ); ?></span>
+            </a>                        
+            <meta itemprop="position" content="2">
+          </li>
+          <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem" class="breadcrumbs_item text-gray-300 px-4">
+            <span itemprop="name"><?php the_title(); ?></span>
+            <meta itemprop="position" content="3" />
+          </li>
+        </ul>
+      </div>
+      <!-- END Хлебные крошки -->
       <!-- TOP CONTENT -->
       <div class="flex flex-col xl:flex-row xl:-mx-10 mb-20">
         <div class="w-full xl:w-1/2 xl:px-10 mb-12 xl:mb-0">
-          <!-- Хлебные крошки -->    
-          <div class="breadcrumbs text-sm text-gray-800 mb-4" itemprop="breadcrumb" itemscope itemtype="https://schema.org/BreadcrumbList">
-            <ul class="flex items-center flex-wrap -mx-4">
-              <li itemprop='itemListElement' itemscope itemtype='https://schema.org/ListItem' class="breadcrumbs_item px-4 pl-8 ml-4">
-                <a itemprop="item" href="<?php echo home_url(); ?>" class="text-orange-400 dark:text-orange-200">
-                  <span itemprop="name"><?php _e( 'Головна', 'treba-wp' ); ?></span>
-                </a>                        
-                <meta itemprop="position" content="1">
-              </li>
-              <li itemprop='itemListElement' itemscope itemtype='http://schema.org/ListItem' class="breadcrumbs_item px-4">
-                <a itemprop="item" href="<?php echo get_post_type_archive_link('products'); ?>" class="text-orange-400 dark:text-orange-200">
-                  <span itemprop="name"><?php _e( 'Обладнання', 'treba-wp' ); ?></span>
-                </a>                        
-                <meta itemprop="position" content="2">
-              </li>
-              <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem" class="breadcrumbs_item text-gray-300 px-4">
-                <span itemprop="name"><?php the_title(); ?></span>
-                <meta itemprop="position" content="3" />
-              </li>
-            </ul>
-          </div>
-          <!-- END Хлебные крошки -->
           <h1 class="text-4xl font-title mb-6"><?php the_title(); ?></h1>
           <div class="content mb-12">
             <?php the_content(); ?>
@@ -49,7 +49,7 @@
         </div>
         <div class="w-full xl:w-1/2 xl:px-10">
           <div>
-            <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" class="w-full h-[400px] object-cover relative rounded-lg" loading="lazy">
+            <img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" class="w-full object-contain relative rounded-lg" loading="lazy">
           </div>
         </div>
       </div>
@@ -81,6 +81,8 @@
           </div>
         <?php endforeach; ?>
       </div>
+      <!-- GALLERY -->
+      <?php if (carbon_get_the_post_meta('crb_product_gallery')): ?>
       <div class="mb-32">
         <div class="text-3xl font-title mb-6"><?php _e("Галерея", "treba-wp"); ?></div>
         <div class="flex flex-wrap -mx-2 xl:-mx-4">
@@ -97,9 +99,9 @@
             </div>
           <?php endforeach; ?>
         </div>
-        
       </div>
-      
+      <?php endif; ?>
+      <!-- END GALLERY -->
     </div>
     <!-- Other products -->
     <div class="container">
